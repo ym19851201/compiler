6f276e3c8f827365188b544cbc1c27328a642026
Miyazaki <miyazaki19851201@gmail.com>
2019-01-19 11:16:05 +0000

    Implement Vector

--- 9cc.c
+++ 9cc.c
@@ -22,11 +22,21 @@ typedef struct {
   char *input;
 } Token;
 
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
 int pos = 0;
 
 Node *add();
 Node *mul();
 Node *term();
+Vector *new_vector();
+void vec_push(Vector *vec, void *elem);
+void error(char* msg, char* input);
+int expect(int line, int expected, int actual);
 
 Token tokens[100];
 
@@ -160,7 +170,51 @@ void error(char* msg, char* input) {
   exit(1);
 }
 
+Vector *new_vector() {
+  Vector *vec = malloc(sizeof(Vector));
+  vec->data = malloc(sizeof(void *) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+  return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+  }
+  vec->data[vec->len++] = elem;
+}
+
+int expect(int line, int expected, int actual) {
+  if (expected == actual)
+    return;
+
+  fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+  exit(1);
+}
+
+void runtest() {
+  Vector *vec = new_vector();
+  expect(__LINE__, 0, vec->len);
+
+  for (int i = 0; i < 100; i++)
+    vec_push(vec, (void *)i);
+
+  expect(__LINE__, 100, vec->len);
+  expect(__LINE__, 0, (int)vec->data[0]);
+  expect(__LINE__, 50, (int)vec->data[50]);
+  expect(__LINE__, 99, (int)vec->data[99]);
+
+  printf("OK\n");
+}
+
 int main(int argc, char **argv) {
+  if (strcmp(argv[1], "-test") == 0) {
+    runtest();
+    return 0;
+  }
+
   if (argc != 2) {
     fprintf(stderr, "The number of arguments is incorrect.\n");
     return 1;
